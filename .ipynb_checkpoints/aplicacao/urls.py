4283baf536501f5057e281cb2a4975ffb8e8a33d
LUCAS KRETSCHMAR <lucas15kretschmar@gmail.com>
2026-02-25 18:35:00 -0300

    adicionado uma tabela de produtos com salvar editar e excluir(CRUD)

--- .ipynb_checkpoints/aplicacao/urls.py
+++ .ipynb_checkpoints/aplicacao/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index, contato
+from .views import index, contato, produto, cadastrarProduto, salvarProduto, editarProduto, salvarProduto
 
 urlpatterns = [
     path('', index),
     path('contato', contato),
+    path('produto', produto, name='urlproduto'),
+    path('cadastrarProduto', cadastrarProduto, name="urlcadastrarProduto"),
+    path('salvarProduto', salvarProduto, name="urlsalvarProduto"),
+    path('editarProduto/<int:id>', editarProduto, name='urleditarProduto'),
+    path('salvarProduto', salvarProduto, name='urlsalvarProduto'),
 ]
